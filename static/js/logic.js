b039d5cffe27903c1f1aa6df65568a9edc733e4e
Andrew Baker <abaker178@gmail.com>
2021-03-28 13:46:44 -0400

    add createMap function

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,3 +14,29 @@ d3.json(queryURL, data => {
     // Send earthquake layer to the createMap function
     createMap(quakes);
 });
+
+// Function - generate map based on provided layer
+function createMap(quakes) {
+    // streetmap layer
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+  // baseMaps object to hold our base layers
+  var baseMaps = { "Street Map": streetmap};
+
+  // overlayMaps object to hold our base layers
+  var overlayMaps = { "Earthquakes": quakes};
+
+  // generate map
+  var myMap = L.map("mapid", {
+    center: [ 37.09, -95.71],
+    zoom: 5,
+    layers: [streetmap, quakes]
+  });
+}
