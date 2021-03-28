9c7311085946dd7354f86e29df327da3f589e303
Andrew Baker <abaker178@gmail.com>
2021-03-28 16:57:30 -0400

    updated markers to circles || added size and color dimensions

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,17 +2,51 @@
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 d3.json(queryURL, data => {
-    
-    // Function - bind a popup describing the place and time of the earthquake to each feature in the geojson
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-    };
+    console.log(data);
 
-    // Function - create GeoJSON layer containing the data's features array
-    var quakes = L.geoJSON(data, {onEachFeature: onEachFeature});
+    let features = data.features;
+    let quakes = [];
 
+    const _sizeRef = 20000;
+    for (let feature of features) {
+        let lat = feature.geometry.coordinates[1],
+            long = feature.geometry.coordinates[0],
+            depth = feature.geometry.coordinates[2],
+            mag = feature.properties.mag;
+        
+        quakes.push(
+            L.circle([lat, long], {
+                color: "black",
+                fillColor: getColor(depth),
+                weight: 1,
+                fillOpacity: 1,
+                radius: mag * _sizeRef
+            }).bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`)
+        );
+    }
+
+    function getColor(depth) {
+        switch (true) {
+            case depth < 10:
+                return "#A3F600";
+            case depth < 30:
+                return "#DCF400";
+            case depth < 50:
+                return "#F7DB11";
+            case depth < 70:
+                return "#FDB72A";
+            case depth < 90:
+                return "#FCA35D";
+            case depth >= 90:
+                return "#FF5F65";
+            default:
+                return "000000";
+        };
+    }
+
+    quakeLayer = L.layerGroup(quakes)
     // Send earthquake layer to the createMap function
-    createMap(quakes);
+    createMap(quakeLayer);
 });
 
 // Function - generate map based on provided layer
@@ -35,8 +69,11 @@ function createMap(quakes) {
 
   // generate map
   var myMap = L.map("mapid", {
-    center: [ 37.09, -95.71],
-    zoom: 5,
+    center: [37.0902, -95.7129],
+    zoom: 4,
     layers: [streetmap, quakes]
   });
+
+  // layer control
+  L.control.layers(baseMaps, overlayMaps, { collapsed: false}).addTo(myMap);
 }
